9c1219e934b842be25597addec589169192f3dd8
Kavin Kumar <111187486+kavinkumarmuthusamy@users.noreply.github.com>
2023-09-19 15:42:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,20 +20,44 @@ function check() {
 }
 
 // get start fumction
-    
-function summit(){
-    let a = document.getElementById("us").value;
-    let b = document.getElementById("pass").value;
-    let c="kavin";
-    let d="Kavin@1234";
-    if(a==c&&b==d){
-        location.replace("demo.html");
-    }else{
-        alert("It is not correct user name and password");
-    }
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz15HWw775zclHeCF40rp1smc3ali57TkO5DdNUGyVm3jKij532aC-0zf4_dFFwo7FD/exec';
+const form = document.forms['validate'];
+const dataList = document.getElementById('data-list');
+
+var object={}
+form.addEventListener('submit', e => {
+e.preventDefault();
+const formData = new FormData(form);
+const email = formData.get('user'); 
+const password = formData.get('pass'); // Get the email value from the form
+
+// Check if the email is already in the data fetched from the Google Apps Script
+if (checkEmailInData(email,password)) {
+alert("Email already exists. Please use a different email.");
+} else {
+   alert("Invalid user name");
+}
+});
+// Function to check if email exists in the data fetched from the Google Apps Script
+function checkEmailInData(email,password) {
+const dataListItems = object;
+for (let i = 0; i < dataListItems.length; i++) {
+const a=dataListItems[i].email;
+const b=dataListItems[i].password;
+// const listItemText = dataListItems[i].textContent;
+if (a==email&&b==password) {
+return true; // Email already exists
+}
+}
+return false; // Email not found
 }
 
-
-
-
+fetch(scriptURL)
+.then(response => response.json())
+.then(data => {
+object=data;
+})
+.catch(error => {
+console.error('Error fetching data:', error);
+});
